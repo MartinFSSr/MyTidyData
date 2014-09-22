66d24e689c5177142ab62b045e11a68af41125e5
Martin Smith <martin.smith@acm.org>
2014-09-21 20:13:40 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,11 @@
+
 # Prepares tidy dataset from Human Activity Recognition Using Smartphones Data Set project
 
-run_analysis <- function() {
+# Usage: x <- run_analysis(WRITE=T)
+# function returns a data frame of means of all observational variables by Measure, Subject and Activity
+# if WRITE=T is specified, a text file "TidyHARMeans.txt" will also be created in the working directory.
+
+run_analysis <- function(WRITE=F) {
 
 # read in features.txt dataset for use as column names
   Feat = read.table("features.txt", sep=" ", col.names=c("Fnum","Feature"))
@@ -34,11 +39,28 @@ run_analysis <- function() {
                266:271,345:350,424:429,503,504,516,517,529,530,542,543,562,563)
   
   
-  tidyData1 <- alldat[,goodCols]
-  
-  
-  }
-  
-  
-  
-  
+  td1 <- alldat[,goodCols]
+
+#Next task: summarize (i.e., means) the data by Activity, Subject and Measure
+# Measures are the observed means and std deviations measurement variables)
+
+library(reshape2)
+
+m1 = melt(td1,id=c(67,68),measure.vars=c(1:66))
+
+# Now used aggregate() function to compute means for all combinations  ...
+
+ag = aggregate(m1, by=list(m1$Subject,m1$Activity,m1$variable), FUN="mean")
+
+# And clean up (eliminate superflous variables and prettify Names)
+
+ag <- ag[,c(3,4,5,7)]
+names(ag)[1] = "Measure"
+names(ag)[4] = "Arithmetic Mean"
+
+# and finally, if WRITE=T, write out the data frame as a txt file
+
+if (WRITE) {x = write.table(ag, "TidyHARMeans.txt", row.names=F)}
+
+return(ag)
+
